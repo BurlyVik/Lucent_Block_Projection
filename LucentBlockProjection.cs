ee4016f2ae7f9c52d9c7606b69673417436587d6
BurlyVik <161164589+BurlyVik@users.noreply.github.com>
2025-08-04 23:45:06 -0400

    Add files via upload

---
+++ LucentBlockProjection.cs
@@ -0,0 +1,113 @@
+﻿using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+using System.Collections.Generic;
+using VRage.Game.Components;
+using VRage.Game.Entity;
+using VRageMath;
+using VRage.Game.ModAPI;
+
+namespace TestEnv
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation | MyUpdateOrder.AfterSimulation)]
+    public class LucentBlockProjection : MySessionComponentBase
+    {
+        public static LucentBlockProjection Instance;
+        
+        BoundingSphereD sphere;
+        GetPlayerCameraSphereD boundingSphere;
+        public IMyCubeGrid Grid;
+        public bool IsPressed { get; set; }
+        /// <summary>
+        /// Get list of entities from player camera sphere
+        /// </summary>
+        private readonly List<MyEntity> _entList = new List<MyEntity>();
+
+        /// <summary>
+        /// Keep track of ticks, helps to reduce # of updates
+        /// </summary>
+        private long Ticks = 0;
+
+        public override void UpdateAfterSimulation()
+        {
+            Instance = this;
+
+            Ticks++; // Count server ticks
+                   
+            // Slow down updates
+            if (Ticks % 120 == 0)
+            {
+                new GetPlayerCameraSphereD(MyAPIGateway.Session.Camera, 10, out sphere);
+                MyGamePruningStructure.GetAllTopMostEntitiesInSphere(ref sphere, _entList);
+
+                foreach (MyEntity entity in _entList)
+                {
+                    // if (entity is IMyCubeGrid && !_entityIds.Contains(entity.EntityId))
+                    if (entity is IMyCubeGrid)
+                    {
+                        // Add cube grid in range of 10 meters 
+                        Grid = (IMyCubeGrid)entity;
+                    }
+                }
+
+                // Lets find the appropriate blocks.
+                foreach (MyCubeBlock cube in Grid.GetFatBlocks<IMyCubeBlock>())
+                {
+                    if (cube != null && cube is IMyConveyorTube   ||
+                                        cube is IMyConveyorSorter ||
+                                        cube is IMyConveyor       ||
+                                        cube is IMyRefinery       ||
+                                        cube is IMyAssembler      || 
+                                        cube is IMyCargoContainer ||
+                                        cube is IMyGasGenerator   ||
+                                        cube is IMyGasTank        ||
+                                        cube is IMyReactor        
+                                        )
+                    {
+                        if (IsPressed == true )
+                        {
+                            if (!cube.IsFunctional)
+                                EnableBlockHighlightRepair(cube);
+                            else
+                                EnableBlockHighlightHealthy(cube);
+                        }
+                        else
+                        {
+                            DisableBlockHighlight(cube);
+                        }
+                    }                    
+                }
+
+                // Whats going on? (Custom, Ingame Breakpoint)
+                // MyAPIGateway.Utilities.ShowMessage("", $"");
+            }
+        }
+
+        /// <summary>
+        /// Enable highlight if block damaged, set health status color code
+        /// </summary>
+        /// <param name="block"></param>
+        public void EnableBlockHighlightRepair(IMyCubeBlock block)
+        {
+            MyVisualScriptLogicProvider.SetHighlight(block.Name, true, 2, 0, Color.IndianRed, -1);
+        }
+
+        /// <summary>
+        /// Enable highlight if block healthy
+        /// </summary>
+        /// <param name="block"></param>
+        public void EnableBlockHighlightHealthy(IMyCubeBlock block)
+        {
+            MyVisualScriptLogicProvider.SetHighlight(block.Name, true, 1, 0, Color.LightGray, -1);
+        }
+
+        /// <summary>
+        /// Disable Block highlight if PowerRequirement not met
+        /// </summary>
+        /// <param name="block"></param>
+        public void DisableBlockHighlight(IMyCubeBlock block)
+        {
+            MyVisualScriptLogicProvider.SetHighlight(block.Name, false, 0, 0, Color.LightGray, -1);
+        }
+    }
+}
---
+++ RadioLucent.cs
@@ -0,0 +1,80 @@
+﻿using Sandbox.ModAPI;
+using SpaceEngineers.Game.ModAPI;
+using VRage.Game.Entity.UseObject;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRageMath;
+
+namespace TestEnv
+{
+    /// <summary>
+    /// Radiolucent – Refers to structures that are less dense and permit
+    /// the x-ray beam to pass through them. Radiolucent structures appear
+    /// dark or black in the radiographic image.
+    /// </summary>
+    /// 
+    [MyUseObject("lucent")]
+    class RadioLucent : MyUseObjectBase
+    {
+        MatrixD InteractionMatrix;        
+        public readonly Matrix LocalMatrix;
+        public readonly IMyButtonPanel ButtonPanel;
+
+        // Used for Instance between classes
+        public bool IsPressed { get; set; }
+
+        // Overrides for interaction matrix
+        public override float InteractiveDistance => 0;
+        public override UseActionEnum PrimaryAction => UseActionEnum.Manipulate;
+        public override UseActionEnum SecondaryAction => UseActionEnum.OpenTerminal;
+        public override MatrixD ActivationMatrix => InteractionMatrix;
+        
+        // Object for session comp.
+        public static LucentBlockProjection instance = LucentBlockProjection.Instance;
+
+        // Implement overrides for Button down, button up.
+        public override UseActionEnum SupportedActions => UseActionEnum.Manipulate
+                                                        | UseActionEnum.UseFinished; // gets called when releasing manipulate
+        
+        public RadioLucent(IMyEntity owner, string dummyName, IMyModelDummy dummyData, uint shapeKey) : base(owner, dummyData)
+        {
+            ButtonPanel = owner as IMyButtonPanel;
+            LocalMatrix = owner.LocalMatrix;
+            InteractionMatrix = ActivationMatrix;
+        }
+
+        public override MyActionDescription GetActionInfo(UseActionEnum actionEnum)
+        {
+            switch (actionEnum)
+            {
+                default:
+                    return default(MyActionDescription);
+            }
+        }
+
+        public override void Use(UseActionEnum actionEnum, IMyEntity user)
+        {
+            // Use for breakpoint;
+            // MyAPIGateway.Utilities.ShowNotification($"Use() action={actionEnum}; user={user}");
+
+            switch (actionEnum)
+            {
+                case UseActionEnum.Manipulate:
+                    if (!IsPressed)
+                    {
+                        MyAPIGateway.Utilities.ShowNotification("Radiolucent Projection Enabled");
+                        instance.IsPressed = true;
+                        IsPressed = !IsPressed;
+
+                    }
+                    else
+                    {
+                        MyAPIGateway.Utilities.ShowNotification("Radiolucent Projection Disabled");
+                        instance.IsPressed = false;
+                        IsPressed = !IsPressed;
+                    }
+                    break;
+            }            
+        }
+    }
+}
